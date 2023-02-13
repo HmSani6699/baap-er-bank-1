2f676a4dc9edab51c3bc0ee51e52e21c59212501
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-13 17:56:16 +0600

    check the all file

--- js/deposit.js
+++ js/deposit.js
@@ -1,42 +1,18 @@
-function getInputValue(inputFiledId) {
-    let depositInputFiled = document.getElementById(inputFiledId);
-    let depositInputFiledValueString = depositInputFiled.value;
-    let depositInputFiledValue = parseFloat(depositInputFiledValueString);
-    depositInputFiled.value = '';
-    return depositInputFiledValue;
-}
-
-function getElementBalance(element) {
-    const depositElement = document.getElementById(element);
-    const depositElementValueString = depositElement.innerText;
-    const depositElementValue = parseFloat(depositElementValueString);
-    return depositElementValue;
-}
-
-function setAmount(elementId, num) {
-    document.getElementById(elementId).innerText = num;
-}
-
-
-//step 1. add event handler to the deposit button
 document.getElementById('deposit-btn').addEventListener('click', function () {
 
-    //step 2. get the deposit input filed value
     const depositInputValue = getInputValue('deposit-field');
 
     if (isNaN(depositInputValue) || depositInputValue < 0) {
         return alert('please provide your number')
     }
-    //step 3. get the previous deposit amount
-    const depositAmountValue = getElementBalance('deposit-total');
 
-    //step 4. set the new deposit amount
+    const depositAmountValue = elementAmount('deposit-total');
+
     const newDepositValue = depositAmountValue + depositInputValue;
-    setAmount('deposit-total', newDepositValue);
+    setElementAmount('deposit-total', newDepositValue);
 
-    //step 5.
-    const previousBalance = getElementBalance('balance-total');
+    const previousBalance = elementAmount('balance-total');
     const newBalance = previousBalance + depositInputValue
-    setAmount('balance-total', newBalance);
+    setElementAmount('balance-total', newBalance);
 
 })
--- js/utiliti.js
+++ js/utiliti.js
@@ -1,1 +1,18 @@
-// console.log(111111111)
+function getInputValue(inputId) {
+    const inputFiled = document.getElementById(inputId);
+    const inputValueString = inputFiled.value;
+    const inputValue = parseFloat(inputValueString);
+    inputFiled.value = '';
+    return inputValue;
+}
+
+function elementAmount(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const elementValue = parseFloat(elementValueString);
+    return elementValue
+}
+
+function setElementAmount(element, num) {
+    document.getElementById(element).innerText = num
+}
